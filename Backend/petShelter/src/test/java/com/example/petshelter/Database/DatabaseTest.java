83f0e7c9fdf6c224766e457604e3446d92dd6414
marshelino-maged <maromaged169@gmail.com>
2023-12-30 03:46:42 +0200

    Update DatabaseTest.java

--- Backend/petShelter/src/test/java/com/example/petshelter/Database/DatabaseTest.java
+++ Backend/petShelter/src/test/java/com/example/petshelter/Database/DatabaseTest.java
@@ -2,6 +2,7 @@ package com.example.petshelter.Database;
 
 import com.example.petshelter.Utilities.Database;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -9,11 +10,13 @@ import static org.junit.Assert.assertTrue;
 public class DatabaseTest {
 
     @Test
+    @Disabled
     public void testBackup(){
         assertTrue(Database.Backup());
     }
 
     @Test
+    @Disabled
     public void testRestore(){
         assertTrue(Database.RestoreDB());
     }
